6f0910060fd21c82136279f00c09f499ff9a7d7c
Arindam Majumder <arindammajumder2020@gmail.com>
2023-07-28 21:48:40 +0530

    placeholder blogs added

--- src/Myfeed.jsx
+++ src/Myfeed.jsx
@@ -10,6 +10,12 @@ const Myfeed = () => {
       {/* Left Section */}
       <div className="w-7/12 max-lg:w-full max-lg:p-3 space-y-5">
         <Blogs />
+        <Blogs />
+        <Blogs />
+        <Blogs />
+        <Blogs />
+        <Blogs />
+        <Blogs />
       </div>
       {/* Right Section */}
       <div className="w-3/12 max-lg:hidden space-y-5">
