fc386e65fe097f6c8ab20deb986a01b1f4acc1dd
Arindam Majumder <arindammajumder2020@gmail.com>
2023-07-28 10:41:31 +0530

    (feat):Fixed side navbar issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -92,8 +92,9 @@ export default function Header({ onThemeChange, isDarkTheme }) {
               </ul>
               <div className="py-2 px-3">
                 <button className="flex flex-row items-center gap-2 w-full py-2 px-3 rounded-lg font-sans cursor-pointer transition-colors duration-150 ease-in-out active:bg-slate-100 dark:active:bg-slate-800 md:hover:bg-slate-100 md:dark:hover:bg-slate-800 focus:outline-none text-slate-600 dark:text-slate-300 text-lg font-semibold">
+                  Rix
                   <img src={Rix} alt="" className="h-5" />
-                  <Rix></Rix>
+                  {/* <Rix></Rix> */}
                 </button>
               </div>
             </div>
