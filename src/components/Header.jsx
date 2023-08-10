7bcf2a71f311bd004514211d1289e67c05fa18d6
Arindam Majumder <arindammajumder2020@gmail.com>
2023-08-10 20:14:32 +0530

    (fix):minor fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,13 +56,13 @@ export default function Header({ onThemeChange, isDarkTheme }) {
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setIsOpen(false);
+        setIsOpen(false); // Close the profile dropdown if clicked outside
       }
       if (
         bellDropdownRef.current &&
         !bellDropdownRef.current.contains(event.target)
       ) {
-        setIsBellOpen(false);
+        setIsBellOpen(false); 
       }
     };
 
@@ -107,7 +107,7 @@ export default function Header({ onThemeChange, isDarkTheme }) {
                   <Link to="/explore">Explore</Link>
                 </li>
                 <li className="flex flex-row justify-between items-center gap-2 w-full py-2 px-3 rounded-lg font-sans cursor-pointer transition-colors duration-150 ease-in-out active:bg-slate-100 dark:active:bg-slate-800 md:hover:bg-slate-100 md:dark:hover:bg-slate-800 focus:outline-none text-slate-600 dark:text-slate-300 text-lg font-semibold">
-                  <Link to="/bookmarks">Bookmarks</Link>
+                  <Link to="https://hashnode.com/onboard">Bookmarks</Link>
                 </li>
                 <li className="flex flex-row justify-between items-center gap-2 w-full py-2 px-3 rounded-lg font-sans cursor-pointer transition-colors duration-150 ease-in-out active:bg-slate-100 dark:active:bg-slate-800 md:hover:bg-slate-100 md:dark:hover:bg-slate-800 focus:outline-none text-slate-600 dark:text-slate-300 text-lg font-semibold">
                   <Link to="/more">More </Link>
@@ -116,7 +116,10 @@ export default function Header({ onThemeChange, isDarkTheme }) {
               </ul>
               <div className="py-2 px-3">
                 <button className="flex flex-row items-center gap-2 w-full py-2 px-3 rounded-lg font-sans cursor-pointer transition-colors duration-150 ease-in-out active:bg-slate-100 dark:active:bg-slate-800 md:hover:bg-slate-100 md:dark:hover:bg-slate-800 focus:outline-none text-slate-600 dark:text-slate-300 text-lg font-semibold">
-                  <Link className="flex" to="/rix">
+                  <Link
+                    className="flex"
+                    to="https://hashnode.com/rix?source=nav"
+                  >
                     {" "}
                     Rix
                   </Link>
@@ -145,7 +148,7 @@ export default function Header({ onThemeChange, isDarkTheme }) {
             <Link to="/explore">Explore</Link>
           </button>
           <button className="hover:opacity-50 rounded-full px-4 py-1 text-sm font-semibold">
-            <Link to="/bookmarks">Bookmarks</Link>
+            <Link to="https://hashnode.com/onboard">Bookmarks</Link>
           </button>
           <button className="hover:opacity-50 flex rounded-full px-4 py-2.5 text-sm font-semibold">
             <Link to="/more">More</Link>
@@ -156,7 +159,7 @@ export default function Header({ onThemeChange, isDarkTheme }) {
            text-sm font-semibold"
           >
             <span>
-              <Link to="/rix">Rix</Link>
+              <Link to="https://hashnode.com/rix?source=nav">Rix</Link>
             </span>{" "}
             <span className="bg-blue-600 rounded-full text-xs py-0.5 px-2 text-white">
               New
@@ -206,9 +209,12 @@ export default function Header({ onThemeChange, isDarkTheme }) {
                     around the world. Grow 1% every day.
                   </h3>
 
-                  <button className="px-[18px] w-full py-2.5 rounded-full text-white text-sm font-medium flex items-center justify-center gap-2 bg-blue-600 ">
+                  <Link
+                    to="https://hashnode.com/onboard"
+                    className="px-[18px] w-full py-2.5 rounded-full text-white text-sm font-medium flex items-center justify-center gap-2 bg-blue-600 "
+                  >
                     Let&apos;s Start
-                  </button>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -237,12 +243,18 @@ export default function Header({ onThemeChange, isDarkTheme }) {
                     Takes only a few seconds
                   </h3>
                   <div className="space-y-3">
-                    <button className="px-[18px] w-full py-2.5 rounded-full text-white text-sm font-medium flex items-center justify-center gap-2 bg-blue-600 ">
+                    <Link
+                      to="https://hashnode.com/onboard"
+                      className="px-[18px] w-full py-2.5 rounded-full text-white text-sm font-medium flex items-center justify-center gap-2 bg-blue-600 "
+                    >
                       Sign up
-                    </button>
-                    <button className="px-[18px] text-blue-600 w-full py-2.5 rounded-full  text-sm font-medium flex items-center justify-center gap-2 border border-blue-600 hover:bg-blue-50 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    </Link>
+                    <Link
+                      to="https://hashnode.com/onboard"
+                      className="px-[18px] text-blue-600 w-full py-2.5 rounded-full  text-sm font-medium flex items-center justify-center gap-2 border border-blue-600 hover:bg-blue-50 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700"
+                    >
                       Log in
-                    </button>
+                    </Link>
                   </div>
                 </div>
               </div>
